a92de74493c53c65c4209baf565a8d31ba7d897c
EC2 Default User <ec2-user@ip-172-31-60-97.ec2.internal>
2022-05-11 10:44:50 +0000

    Languages done, need to add destroy action.

--- hr/config/routes.rb
+++ hr/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :skills
   end
   
+  resources :resumes do
+    resources :languages
+  end
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
